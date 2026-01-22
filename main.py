8c3534c6f51160b1a35637af9285b6a963d720ff
Shubham Tiwari <safarnamasuccesska@gmail.com>
2026-01-22 23:59:07 +0530

    Fix main.py to use app.py Flask app

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Top 3 Best Options is running 🚀"
+from app import app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
